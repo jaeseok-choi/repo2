3eb61a226d8f7f7531a628d84460ab8677b2b855
jaeseok choi <wotjrwjdghks@naver.com>
2020-11-28 12:39:16 +0900

    develop program.c modified

--- program.c
+++ program.c
@@ -5,7 +5,12 @@ int sum(int n)
   if (n == 0) return 0;
   return n + sum(n-1);
 }
-
+int fib(int n)
+{
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  return 42;	
+}
 int main()
 {
   printf("Hello world!\n");
